59c56b62b9d555ea43a4baf955c1bc7f2e30daf8
fernandorubini <fernandotestelealapps@gmail.com>
2024-06-20 10:09:16 -0300

    Exercism.org

---
+++ src/Main.kt
@@ -0,0 +1,30 @@
+import java.util.Locale
+
+object Acronym {
+    fun generate(phrase: String) : String {
+        // Remove pontuação, exceto hífens, e converte para maiúsculas
+        val cleanedPhrase = phrase.replace(Regex("[^A-Za-z0-9\\- ]"), "")
+            .uppercase(Locale.getDefault())
+
+        // Divide a frase em palavras, usando espaços e hífens como separadores
+        val words = cleanedPhrase.split(Regex("[\\s\\-]+"))
+
+        // Extrai a primeira letra de cada palavra e junta as letras
+        val acronym = words.map { it.first() }.joinToString("")
+
+        return acronym
+    }
+}
+
+fun main() {
+    val phrases = listOf(
+        "As Soon As Possible",
+        "Liquid-crystal display",
+        "Thank George It's Friday!"
+    )
+
+    for (phrase in phrases) {
+        val acronym = Acronym.generate(phrase)
+        println("$phrase -> $acronym")
+    }
+}
