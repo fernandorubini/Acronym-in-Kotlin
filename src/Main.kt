887d1c3fb823ead14803452a5b070bac0b855cd8
fernandorubini <fernandotestelealapps@gmail.com>
2025-04-16 17:50:23 -0300

    Finalização do projeto Acronym in Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,8 @@ object Acronym {
     fun generate(phrase: String) : String {
         // Remove pontuação, exceto hífens, e converte para maiúsculas
         val cleanedPhrase = phrase.replace(Regex("[^A-Za-z0-9\\- ]"), "")
-            .uppercase(Locale.getDefault())
+            .uppercase(Locale.US // ou Locale("pt", "BR")
+            )
 
         // Divide a frase em palavras, usando espaços e hífens como separadores
         val words = cleanedPhrase.split(Regex("[\\s\\-]+"))
